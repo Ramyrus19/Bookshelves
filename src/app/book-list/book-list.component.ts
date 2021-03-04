e0252170ccd5ff6f17eef50bba8b6698198a9799
ramona <contact.ramonarusanu@gmail.com>
2021-03-04 15:42:57 +0100

    CRUD for Book

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Book} from '../models/book.model';
+import {Subscription} from 'rxjs';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private booksService: BooksService, private router: Router) { }
 
   ngOnInit(): void {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.getBooks();
+    this.booksService.emitBooks();
   }
 
+  // tslint:disable-next-line:typedef
+  onNewBook(){
+    this.router.navigate(['/books', 'new']);
+  }
+
+  // tslint:disable-next-line:typedef
+  onDeleteBook(book: Book){
+    this.booksService.removeBook(book);
+  }
+
+  // tslint:disable-next-line:typedef
+  onViewBook(id: number){
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy(): void {
+    this.booksSubscription.unsubscribe();
+  }
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,73 @@
 import { Injectable } from '@angular/core';
+import {Book} from '../models/book.model';
+import {Subject} from 'rxjs';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
 
   constructor() { }
+
+  // tslint:disable-next-line:typedef
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  // tslint:disable-next-line:typedef
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  // tslint:disable-next-line:typedef
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+
+  // tslint:disable-next-line:typedef
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // tslint:disable-next-line:typedef
+  createNewBook(newBook: Book){
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  // tslint:disable-next-line:typedef
+  removeBook(book: Book){
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book){
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
